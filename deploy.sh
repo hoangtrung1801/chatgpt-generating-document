53f85beb33f44d00a7ae6349331fe3bba64564d0
Hoang Trung <hoangtrung1801.2003@gmail.com>
2023-04-20 07:05:44 +0700

    fix: deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-docker container prune -a -f
-git pull origin develop
+docker container prune -af
+git pull -f origin develop
 docker-compose down --remove-orphans
 docker-compose up -d --build
