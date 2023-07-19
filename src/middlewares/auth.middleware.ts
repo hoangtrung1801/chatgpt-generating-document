3ed119240758e8f162c0d7828bb6c8c32aca6d7f
Hoang Trung <hoangtrung1801.2003@gmail.com>
2023-07-19 21:52:37 +0700

    update: open auth

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -6,34 +6,29 @@ import { HttpException } from "@exceptions/HttpException";
 import { DataStoredInToken, RequestWithUser } from "@interfaces/auth.interface";
 
 const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
-  const users = new PrismaClient().user;
-  const findUser = await users.findUnique({ where: { id: 7 } });
-  req.user = findUser;
-  next();
+  try {
+    const Authorization = req.cookies["Authorization"] || (req.header("Authorization") ? req.header("Authorization").split("Bearer ")[1] : null);
 
-  // try {
-  //   const Authorization = req.cookies["Authorization"] || (req.header("Authorization") ? req.header("Authorization").split("Bearer ")[1] : null);
+    if (Authorization) {
+      const secretKey: string = SECRET_KEY;
+      const verificationResponse = (await verify(Authorization, secretKey)) as DataStoredInToken;
+      const userId = verificationResponse.id;
 
-  //   if (Authorization) {
-  //     const secretKey: string = SECRET_KEY;
-  //     const verificationResponse = (await verify(Authorization, secretKey)) as DataStoredInToken;
-  //     const userId = verificationResponse.id;
+      const users = new PrismaClient().user;
+      const findUser = await users.findUnique({ where: { id: Number(userId) } });
 
-  //     const users = new PrismaClient().user;
-  //     const findUser = await users.findUnique({ where: { id: Number(userId) } });
-
-  //     if (findUser) {
-  //       req.user = findUser;
-  //       next();
-  //     } else {
-  //       next(new HttpException(401, "Wrong authentication token"));
-  //     }
-  //   } else {
-  //     next(new HttpException(404, "Authentication token missing"));
-  //   }
-  // } catch (error) {
-  //   next(new HttpException(401, "Wrong authentication token"));
-  // }
+      if (findUser) {
+        req.user = findUser;
+        next();
+      } else {
+        next(new HttpException(401, "Wrong authentication token"));
+      }
+    } else {
+      next(new HttpException(404, "Authentication token missing"));
+    }
+  } catch (error) {
+    next(new HttpException(401, "Wrong authentication token"));
+  }
 };
 
 export default authMiddleware;
